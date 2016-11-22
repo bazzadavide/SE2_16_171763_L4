2da013fc8272097f5851d0fc709cc852d4ce9ad1
bazza <davide.bazzanella-2@studenti.unitn.it>
2016-11-23 00:04:14 +0100

    test the post function, still need a code clean

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var level = [];
 var salary = [];
 var cont = 0;
 var lock = 1;
+var qs = require('querystring');
 //express lib
 var express = require('express');
 //inspect
@@ -17,10 +18,11 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set('port', (process.env.PORT || 1337));
+app.set('port', (process.env.PORT || 1338));
 
 app.use('/', function(request, response) 
 {
+    
     //set the headers of the responce
     var headers = {};
     //answer
@@ -33,22 +35,26 @@ app.use('/', function(request, response)
 	if ( typeof request.body !== 'undefined' && request.body)
 	{
         //the ontent of the POST receiced
-		text = "request.body: " + util.inspect(request.body) + "\n";	
+		text = "request.body: " + util.inspect(request.body.id) + "\n";	
 		//se definita e non nulla prendo l'elemento id dal form
 		if ( typeof request.body.id !== 'undefined' && request.body.id){
             //salvo nell'array l'id
-			id[cont] = request.body.id;
+            console.log("if dell'id");
+			id[cont] = JSON.stringify(request.body.id);
+            console.log(id[cont]);
 		
             //oltre a fare il controllo di prima controlla anche una variabile lock, senza ID non esiste l'operaio
-		  if ( typeof request.body.name !== 'undefined' && request.body.name)
+		  if ( typeof request.body.name !== 'undefined' && request.body.name){
                 //salvo nell'array 
-    		  name[cont] = request.body.name;
+    		  name[cont] = JSON.stringify(request.body.name);
+              console.log("if del name");
+          }
 		  else 
                 // in caso qualcosa vada storto
 			  name[cont] = "not defined";
           if ( typeof request.body.surname !== 'undefined' && request.body.surname)
                 //salvo nell'array 
-              surname[cont] = request.body.surname;
+              surname[cont] = JSON.stringify(request.body.surname);
 		  else 
                 // in caso qualcosa vada storto
 			  surname[cont] = "not defined";
@@ -67,10 +73,9 @@ app.use('/', function(request, response)
             
             cont++;
         }
-        else 
-			alert("Non è stato selezionato alcun ID per l'operaio");
         
-	    	
+        
+	    console.log(id[cont]);	
         text = text + "post received: id=" + id[cont] + ", name="+ name[cont]+", surname="+surname[cont]+", level="+level[cont]+", salary="+salary[cont];
 	}
 	else
